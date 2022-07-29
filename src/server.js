a355c99539722d6156366e0ccd36a30d5b97bd65
Seokju Hong <iamseokjuhong@gmail.com>
2022-07-29 14:59:38 +0900

    WebSockets in NodeJS
    웹소켓 설치: npm i ws

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -6,7 +8,11 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`)
 
-app.listen(3000);
+const server = http.createServer(app); // http Server
+const wss = new WebSocket.Server({server});
+
+server.listen(3000, handleListen);
