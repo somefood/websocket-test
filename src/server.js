c8ef5819eda0d93702a41431fc442c4f95a94c69
Seokju Hong <iamseokjuhong@gmail.com>
2022-07-31 23:12:50 +0900

    User Count

--- src/server.js
+++ src/server.js
@@ -31,6 +31,10 @@ function publicRooms() {
     return publicRooms;
 }
 
+function countRoom(roomName) {
+    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
+
 wsServer.on("connection", (socket) => {
     socket["nickname"] = "Anon";
     socket.onAny((event) => {
@@ -41,11 +45,11 @@ wsServer.on("connection", (socket) => {
         socket.join(roomName);
         done();
         console.log(socket.rooms);
-        socket.to(roomName).emit("welcome", socket.nickname);
+        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
         wsServer.sockets.emit("room_change", publicRooms());
     });
     socket.on("disconnecting", () => {
-        socket.rooms.forEach(room => socket.to(room).emit("bye", socket.nickname));
+        socket.rooms.forEach(room => socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1));
     });
     socket.on("disconnect", () => {
         wsServer.sockets.emit("room_change", publicRooms());
