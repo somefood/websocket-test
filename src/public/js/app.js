d25be1e291bdccec92c5a4f10d62a4f72676ee07
Seokju Hong <iamseokjuhong@gmail.com>
2022-08-04 12:12:45 +0900

    Call Controls

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,10 +3,26 @@ const socket = io();
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
 
 let myStream;
 let muted = false;
 let cameraOff = false;
+
+async function getCameras() {
+    try {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter((device) => device.kind === "videoinput");
+        cameras.forEach((camera) => {
+            const option = document.createElement("option");
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
+            camerasSelect.appendChild(option); 
+        });
+    } catch(e) {
+        console.log(e);
+    }
+}
 
 async function getMedia() {
     try{
@@ -23,9 +39,14 @@ async function getMedia() {
 }
 
 getMedia();
-
+getCameras();
 
 function handleMuteBtnClick() {
+    myStream
+    .getAudioTracks()
+    .forEach((track) => {
+        track.enabled = !track.enabled;
+    });
     if(!muted) {
         muteBtn.innerText = "Unmute";
         muted = true;
@@ -36,6 +57,12 @@ function handleMuteBtnClick() {
 }
 
 function handleCameraBtnClick() {
+    console.log(myStream.getVideoTracks());
+    myStream
+    .getVideoTracks()
+    .forEach((track) => {
+        track.enabled = !track.enabled;
+    });
     if(!cameraOff) {
         cameraBtn.innerText = "Turn Camera Off";
         cameraOff = true;
