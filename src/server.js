b7e31f2f8f5ee8f74be9d10cf56c40e28e65fca8
Seokju Hong <iamseokjuhong@gmail.com>
2022-08-04 14:02:11 +0900

    Answers

--- src/server.js
+++ src/server.js
@@ -15,13 +15,15 @@ const httpServer = http.createServer(app); // http Server
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-    socket.on("join_room", (roomName, done) => {
+    socket.on("join_room", (roomName) => {
         socket.join(roomName);
-        done();
         socket.to(roomName).emit("welcome");
     });
     socket.on("offer", (offer, roomName) => {
         socket.to(roomName).emit("offer", offer);
+    });
+    socket.on("answer", (answer, roomName)=> {
+        socket.to(roomName).emit("answer", answer);
     });
 });
 
