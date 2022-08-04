4bc4e1389f060149fd3d46da6053be3f7bf4223f
Seokju Hong <iamseokjuhong@gmail.com>
2022-08-04 14:51:34 +0900

    senders

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -89,6 +89,13 @@ function handleCameraBtnClick() {
 
 async function handleCameraChange() {
     await getMedia(camerasSelect.value);
+    if (myPeerConnection) {
+        const videoTrack = myStream.getVideoTracks()[0];
+        const videoSender = myPeerConnection
+                            .getSenders()
+                            .find((sender) => sender.track.kind === "video");
+        videoSender.replaceTrack(videoTrack);
+    }
 }
 
 muteBtn.addEventListener("click", handleMuteBtnClick);
